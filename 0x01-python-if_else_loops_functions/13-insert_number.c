e9eff1733679186c4919a117a6df0ea34641eb31
FrankDonny <arkohfrank1@gmail.com>
2022-06-28 23:05:10 +0000

    if_else & loops

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newnode, *temp
+
+	newnode = malloc(sizeof(listint_t))
+	temp = head;
+	while ()
+	
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    for j in str(i):
-        if len(str(i)) == 1:
-            print("{:02d}".format(int(j), end=", ")
-                    elif len(str(i)) == 2 and j == (str(i)[0:1} str(i)[1]:
-            print("{:02d}".format(int(j), end="")
+for i in range(0, 10):
+    for j in range(0, 10):
+        print("{}{}".format(i, j), end="")
+
