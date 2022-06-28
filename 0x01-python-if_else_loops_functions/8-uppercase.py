2c503e1aaea703bbb36d843c7c02295003312b0b
FrankDonny <arkohfrank1@gmail.com>
2022-06-28 16:29:01 +0000

    if_else & loops

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for i in str:
+        if ord(i) in range(97, 123):
+            caps = ord(i) - 32
+            result += chr(caps)
+        else:
+            result += i
+    print(result)
