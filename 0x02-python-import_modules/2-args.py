a5a813f8616c23331aaf23d70572e8ed617385a6
FrankDonny <arkohfrank1@gmail.com>
2022-06-30 21:01:40 +0000

    import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,19 +7,20 @@ def arg():
     return length - 1
 
 
-count = 0
+if __name__ == "__main__":
+    count = 0
 
-if len(sys.argv) <= 1:
-    print("{} arguments.".format(arg()))
-elif len(sys.argv) == 2:
-    count += 1
-    print("{} argument:".format(arg()))
-    print("{}: {}".format(count, sys.argv[1]))
-else:
-    print("{} arguments:".format(arg()))
-    for i in sys.argv:
-        if i == sys.argv[0]:
-            continue
-        if len(sys.argv) > 2:
-            count += 1
-            print("{}: {}".format(count, i))
+    if len(sys.argv) <= 1:
+        print("{} arguments.".format(arg()))
+    elif len(sys.argv) == 2:
+        count += 1
+        print("{} argument:".format(arg()))
+        print("{}: {}".format(count, sys.argv[1]))
+    else:
+        print("{} arguments:".format(arg()))
+        for i in sys.argv:
+            if i == sys.argv[0]:
+                continue
+            if len(sys.argv) > 2:
+                count += 1
+                print("{}: {}".format(count, i))
