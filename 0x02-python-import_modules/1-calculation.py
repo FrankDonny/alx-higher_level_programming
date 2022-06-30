5155fc809054ed962623a4cd85c89cb53eb73589
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-06-30 22:37:04 +0000

    import modules
    import modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,6 +5,8 @@ import calculator_1
 if __name__ == '__main__':
     a = 10
     b = 5
+    
+    
     print("10 + 5 = {}".format(calculator_1.add(a, b)))
     print("10 - 5 = {}".format(calculator_1.sub(a, b)))
     print("10 * 5 = {}".format(calculator_1.mul(a, b)))
