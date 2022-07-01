cae17887e4ef62db1beb94b964cd1012cba47790
FrankDonny <arkohfrank1@gmail.com>
2022-07-01 10:30:12 +0000

    import and modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 import sys
+from sys import exit
 
-if __name__ == "__main_":
-    length = len(sys.argv)
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-    for i in range(0, length):
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[i] == sys.argv[0]:
-            continue
-        elif length != 3:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            exit(1)
-        elif lenght == 3:
-            for operator in ["+", "-", "*", "/"]:
-                if operator == '+':
-                    print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-                    #exit(0)
-                elif operator == '-':
-                    print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-                    #exit(0)
-                elif operator == '*':
-                    print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
-                    #exit(0)
-                elif operator == '/':
-                    print("{} {} {} = {}".format(a, operator, b, div(a, b)))
-                    #exit(0)
-                else:
-                    print("Unknown operator. Available operators: +, -, * and /")
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+
+    if operator == '+':
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+        exit(0)
+    elif operator == '-':
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        exit(0)
+    elif operator == '*':
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        exit(0)
+    elif operator == '/':
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+        exit(0)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
