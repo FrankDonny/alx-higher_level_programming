94756e73f26820dc9cc61fe35d2f14dad8ca5f97
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-07-02 17:04:08 +0000

    data structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return
-    for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=" ")
-        print()
+    else:
+        for i in matrix:
+            for j in i:
+                print("{:d}".format(j), end=" ")
+            print()
