84ba80789cc063622adb6e97422132cf42c5af50
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-07-02 21:13:20 +0000

    data structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-def add_tuple(tuple_aa=(), tuple_bb=()):
-    length_a = len(list(tuple_aa)) - 1
-    length_b = len(list(tuple_bb)) - 1
-    if not tuple_aa:
-        tuple_aa = list(tuple_aa) + [0, 0]
+def add_tuple(tuple_a=(), tuple_b=()):
+    length_a = len(list(tuple_a))
+    length_b = len(list(tuple_b))
+    if not tuple_a:
+        tuple_a = list(tuple_a) + [0, 0]
     elif not tuple_bb:
-        tuple_bb = list(tuple_bb) + [0, 0]
+        tuple_b = list(tuple_b) + [0, 0]
     elif length_a < 2:
-        tuple_aa = list(tuple_aa) + [0]
+        tuple_a = list(tuple_a) + [0]
     elif length_b < 2:
-        tuple_bb = list(tuple_bb) + [0]
-    elif len(tuple_aa) > 2 or len(tuple_bb) > 2:
-        tuple_aa = tuple_aa[0:2]
-        tuple_bb = tuple_bb[0:2]
+        tuple_b = list(tuple_b) + [0]
+    elif len(tuple_a) > 2 or len(tuple_b) > 2:
+        tuple_a = tuple_a[0:2]
+        tuple_b = tuple_b[0:2]
 
-    m1 = tuple_aa[0] + tuple_bb[0]
-    m2 = tuple_aa[1] + tuple_bb[1]
+    m1 = tuple_a[0] + tuple_b[0]
+    m2 = tuple_a[1] + tuple_b[1]
     new_list = [m1, m2]
     new_tuple1 = tuple(new_list)
     return new_tuple1
