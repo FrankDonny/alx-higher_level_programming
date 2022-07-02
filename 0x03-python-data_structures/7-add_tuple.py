1028f907939eeaf08582398c278468d399f1cc7a
FrankDonny <arkohfrank1@gmail.com>
2022-07-02 21:27:29 +0000

    data structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,8 +14,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a[0:2]
         tuple_b = tuple_b[0:2]
     else:
-        tuple_a = list((tuple_a[0], tuple_a[1]))
-        tuple_b = list((tuple_b[0], tuple_b[1]))
+        tuple_a = (tuple_a[0], tuple_a[1])
+        tuple_b = (tuple_b[0], tuple_b[1])
+        tuple_a = list(tuple_a)
+        tuple_b = list(tuple_b)
 
     m1 = tuple_a[0] + tuple_b[0]
     m2 = tuple_a[1] + tuple_b[1]
