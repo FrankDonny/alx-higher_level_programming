55acdda74ebcb08273903a32f5fe3c471d10873b
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-07-05 00:42:22 +0000

    data structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,9 +10,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = list(tuple_a) + [0]
     elif length_b < 2:
         tuple_b = list(tuple_b) + [0]
-    elif len(tuple_a) > 2 or len(tuple_b) > 2:
-        tuple_a = tuple_a[0:2]
-        tuple_b = tuple_b[0:2]
+    elif length_a > 2:
+        tuple_a = list(tuple_a[0:2])
+    elif length_b > 2:
+        tuple_b = list(tuple_b[0:2])
     else:
         tuple_a = (tuple_a[0], tuple_a[1])
         tuple_b = (tuple_b[0], tuple_b[1])
