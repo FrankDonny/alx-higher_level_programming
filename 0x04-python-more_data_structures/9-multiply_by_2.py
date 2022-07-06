b1cd9b18868ce509f33f161e729ebfb1a17de5b8
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-07-06 22:00:31 +0000

    more data structures

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -10,9 +10,3 @@ def multiply_by_2(a_dictionary):
     for k, v in a_dictionary.items():
         a_dictionary[k] = v * 2
     return a_dictionary
-
-a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-new_dict = multiply_by_2(a_dictionary)
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
