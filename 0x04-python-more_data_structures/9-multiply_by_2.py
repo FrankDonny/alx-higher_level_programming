61607233676a69052a24a4b86e557e7f13c27601
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-07-06 22:05:05 +0000

    more data structures

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    a_dictionary = dict(a_dictionary)
-
-    for key, value in sorted(a_dictionary.items()):
-        print(f"{key}: {value}")
-
 def multiply_by_2(a_dictionary):
     a_dictionary = dict(a_dictionary)
     for k, v in a_dictionary.items():
