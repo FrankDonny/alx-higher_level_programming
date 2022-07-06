eca8108561ac327b5440cb20785851a8e25f4f9d
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-07-06 23:59:35 +0000

    more data structures

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+
+    new_list = []
+    newlist1 = []
+    sumall = 0
+    sumall1 = 0
+    for mem in my_list:
+        mem = list(mem)
+        newlist1.append(mem[1])
+        total = mem[0] * mem[1]
+        new_list.append(total)
+    for j in newlist1:
+        sumall1 += j
+    for i in new_list:
+        sumall += i
+    result = sumall/sumall1
+    return result, new_list
