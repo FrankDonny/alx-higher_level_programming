18c9ecb40eab1400e515b1296069ee4ee58cc4d6
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-07-07 00:13:43 +0000

    more data structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
+    if bool(my_list) == 0:
         return 0
 
     new_list = []
@@ -17,4 +17,4 @@ def weight_average(my_list=[]):
     for i in new_list:
         sumall += i
     result = sumall/sumall1
-    return result, new_list
+    return result
