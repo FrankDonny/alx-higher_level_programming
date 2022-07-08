0c21b2e99b0880b6967f12f1c1a5aad9a9b8cabe
FrankDonny <arkohfrank1@gmail.com>
2022-07-08 02:35:13 +0000

    more data structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+from string import ascii_letters
 def roman_to_int(roman_string):
-    if roman_string is None:
-        return 0
+    letters = ascii_letters
+    for i in range(0, len(roman_string)):
+        if roman_string[i] not in letters:
+            return 0
     roman_string = str(roman_string).upper()
     my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
