a7d5868b1ddb362fdeb3e75cb40ea6ef39c49931
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-07-19 00:52:22 +0000

    try/except functions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,13 +3,13 @@ import sys
 def safe_print_integer_err(value):
     try:
         if isinstance(value, str):
-            print("Exception: Unknown format code 'd' ", sys.stderr, end="")
-            print("for object of type 'str'", sys.stderr)
+            print("Exception: Unknown format code 'd' ", file=sys.stderr, end="")
+            print("for object of type 'str'", file=sys.stderr)
             return False
         else:
             print("{:d}".format(value))
             return True
     except Exception:
-        print("Exception: Unknown format code 'd' ", sys.stderr, end="")
-        print("for object of type 'str'", sys.stderr)
+        print("Exception: Unknown format code 'd' ", file=sys.stderr, end="")
+        print("for object of type 'str'", file=sys.stderr)
         return False
