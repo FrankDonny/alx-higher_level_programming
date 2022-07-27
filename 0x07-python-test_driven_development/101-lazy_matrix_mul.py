5ee1dae00f1d41c2a4a83bfcd61a27c4e8d13cd3
FrankDonny <arkohfrank1@gmail.com>
2022-07-27 12:45:21 +0000

    Test-driven development

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+This module contain the function lazy_matrix_mul
+"""
 import numpy as np
-"""
-This function multiplies 2 matrices
-with the use of NUMPY
-"""
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """
+    This function multiplies 2 matrices
+    with the use of NUMPY
+    """
+
     aa = np.array(m_a)
     bb = np.array(m_b)
     cc = aa.dot(bb)
