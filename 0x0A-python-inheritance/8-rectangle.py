16296310228b055c4477029011eab37e66f0da6e
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-08-01 20:13:50 +0000

    Python - Inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle(BaseGeometry):
     """Rectangle class inheriting from super class BaseGeometry"""
     def __init__(self, width, height):
         """check validation of the width and height"""
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
