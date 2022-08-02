01750dfc2c801afa1e21814d3aa5009713b34c93
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-08-02 15:37:50 +0000

    Python - Input/Output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,4 @@ import json
 def load_from_json_file(filename):
     """this function reads from a json file"""
     with open(filename, encoding='utf-8') as myFile:
-        new = myFile.read()
-    return json.load(new)
+        return json.load(myFile)
