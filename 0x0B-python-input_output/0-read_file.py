85e479d6a1e2f81a489e897247f7a560948dfc89
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-08-02 15:39:28 +0000

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """This function reads the content of a file"""
     with open(filename, 'r', encoding='utf-8') as theFile:
-        print(theFile.read())
+        print(theFile.read(), end="")
