42e225af2d4bce2016b8b0047464d591e975536c
frankdonny <arkohfrank1@gmail.com>
2022-10-04 01:02:12 +0000

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,9 +2,17 @@
 const args = process.argv;
 function factorial (num) {
   let fact;
-  if (num === 0) {
-    return (fact);
+  if (num === 1) {
+    return (num);
   } else {
     fact = num * factorial(num - 1);
+    return fact;
   }
+}
+
+if (args.length - 2 === 0) {
+  console.log(1);
+} else {
+  const num = factorial(args[2]);
+  console.log(num);
 }
