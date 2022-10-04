48e7d7ab2bd06a8ad7b76c96626d92fa69ff90b6
frankdonny <arkohfrank1@gmail.com>
2022-10-04 23:11:12 +0000

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,31 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if ((w < 1 || h < 1) || (w === undefined || h === undefined)) {
-      /* pass */
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Rectangle = require('./5-square');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    [this.height, this.width] = [this.width, this.height];
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-}
-
-class Square extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
@@ -41,6 +17,4 @@ class Square extends Rectangle {
       }
     }
   }
-}
-
-module.exports = Square;
+};
