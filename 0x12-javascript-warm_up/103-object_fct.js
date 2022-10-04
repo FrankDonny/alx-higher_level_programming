09b00e4cd260c4a231f32418faef008484155fed
frankdonny <arkohfrank1@gmail.com>
2022-10-04 02:02:10 +0000

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
+  value: 12
 };
 console.log(myObject);
 
-function incr () {
-  incr = function () {
-    myObject.value = myObject.value + 1;
-  };
-  myObject.func = incr;
-}
-
-incr();
+myObject.incr = function () {
+  this.value++;
+};
 
 myObject.incr();
 console.log(myObject);
