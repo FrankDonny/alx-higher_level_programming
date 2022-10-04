2ab18260a4300011ca07dccc742ece28fcd4beca
frankdonny <arkohfrank1@gmail.com>
2022-10-04 16:00:15 +0000

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -16,17 +16,12 @@ class Rectangle {
   }
 
   rotate () {
-    for (let i = 0; i < this.width; i++) {
-      console.log('X'.repeat(this.height));
-    }
+    [this.height, this.width] = [this.width, this.height];
   }
 
   double () {
     this.height = this.height * 2;
     this.width = this.width * 2;
-    for (let i = 0; i < (this.height); i++) {
-      console.log('X'.repeat(this.width));
-    }
   }
 }
 
