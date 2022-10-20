9a86a2cb0f5eefce9ca7d78d968c66208672bc44
FrankDonny <arkohfrank1@gmail.com>
2022-10-20 23:24:12 +0000

    0x10. Python - Network #0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL
-curl -X POST --data-binary "email: test@gmail.com&subject: I will always be here for PLD" "$1"
+curl $1 -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
