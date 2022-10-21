fdb9b8dedfde9ed9e48e4183a686afd0dbcfe134
FrankDonny <arkohfrank1@gmail.com>
2022-10-21 23:31:12 +0000

    0x11. Python - Network #1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Python script that takes in a letter and sends a POST
 request to http://0.0.0.0:5000/search_user"""
-from email.mime import application
 import requests
 from sys import argv
 
