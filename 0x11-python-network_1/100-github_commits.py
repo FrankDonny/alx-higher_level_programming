6624f4e2f43af6492f9d67669f01abd0c67dfecf
FrankDonny <arkohfrank1@gmail.com>
2022-10-22 01:22:50 +0000

    0x11. Python - Network #1

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments"""
+from textwrap import indent
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    res = requests.get(f"https://api.github.com/repos/{argv[2]}/{argv[1]}"
+                       "/commits")
+    commits = res.json()
+    i = 0
+    for key in commits:
+        if i >= 10:
+            break
+        print(key.get('sha') + ': ', end="")
+        print(key.get('commit').get('author').get('name'))
+        i += 1
