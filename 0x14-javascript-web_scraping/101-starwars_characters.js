6553817699e755e449e359dbc72fb39efb26127a
frankdonny <arkohfrank1@gmail.com>
2022-11-16 05:51:43 +0000

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,20 +2,36 @@
 const request = require('request');
 const args = process.argv.slice(2);
 const url = `https://swapi-api.hbtn.io/api/films/${args[0]}`;
-request(url, function (err, res, body) {
+request(url, async function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
     const obj = JSON.parse(body);
-    const chars = obj.characters;
-    for (const char in chars) {
-      request(chars[char], function (err, res, body) {
-        if (err) { console.log(err); }
-        const name = JSON.parse(body);
-        console.log(name.name);
-      }
-      );
-      // console.log(chars[char])
+    for (const charURL of obj.characters) {
+      await new Promise((resolve, reject) => {
+        request(charURL, (err, r, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
+      });
     }
+  //   const chars = obj.characters;
+  //   const arr = []
+  //   for (const char in chars) {
+  //     arr.push(chars[char]);
+  //   }
+  //   for (let ele = 0; ele < arr.length; ele++) {
+  //     request(arr[ele], function (err, res, body) {
+  //       if (err) { console.log(err); }
+  //       const name = JSON.parse(body);
+  //       console.log(name.name);
+  //     }
+  //     );
+  //   }
+  //   // console.log(arr)
   }
 });
