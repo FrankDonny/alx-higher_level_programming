02d65644c37f2e4579a81373646c31ad18710852
frankdonny <arkohfrank1@gmail.com>
2022-11-16 05:30:10 +0000

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,16 +6,16 @@ const url = args[0];
 request(url, function (err, res, body) {
   if (err) { console.log(err); } else {
     const completed = {};
-  const jsonBody = JSON.parse(body);
-  for (const task of jsonBody) {
-    if (task.completed) {
-      if (completed[task.userId]) {
-        completed[task.userId]++;
-      } else {
-        completed[task.userId] = 1;
+    const jsonBody = JSON.parse(body);
+    for (const task of jsonBody) {
+      if (task.completed) {
+        if (completed[task.userId]) {
+          completed[task.userId]++;
+        } else {
+          completed[task.userId] = 1;
+        }
       }
     }
-  }
-  console.log(completed);
+    console.log(completed);
   }
 });
